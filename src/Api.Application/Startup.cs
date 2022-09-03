4e90b94c7007f607a772c38989354161e882f1ee
AlvaroAlves <alvaroalves87@gmail.com>
2022-09-03 13:26:01 -0300

    Configuração da injeção de dependencia pt2

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -26,6 +26,8 @@ namespace application
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            ConfigureDatabase.UseSqlServer(services);
+            ConfigureRepository.ConfigureDependenciesRepository(services);
             ConfigureService.ConfigureDependenciesService(services);
             services.AddControllers();
         }
---
+++ src/Api.CrossCuting/DependencyInjection/ConfigureDatabase.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.CrossCuting.DependencyInjection
+{
+    public class ConfigureDatabase
+    {
+        public static void UseSqlServer(IServiceCollection serviceCollection){
+            serviceCollection.AddDbContext<MyContext>(
+                options => options.UseSqlServer("Server=localhost;Database=estudantes;Trusted_Connection=True")
+            );
+        }
+    }
+}
